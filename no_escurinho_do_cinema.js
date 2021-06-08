412436b7a56fa79dd91e8954d1ae1e127c27c302
Lucas Kennedy <lukakfp@gmail.com>
2021-06-08 18:02:53 -0300

    Soluação para o desafio No escurinho do cinema

---
+++ no_escurinho_do_cinema.js
@@ -0,0 +1,8 @@
+function solucao(obj) {
+  if (obj.temIngresso && (obj.idade >= obj.censura || obj.estaComPais)) {
+    if (obj.temCarteirinha || obj.idade < 18) console.log("MEIA");
+    else console.log("INTEIRA");
+  } else {
+    console.log("ACESSO NEGADO");
+  }
+}
